d3bd5d09051b436e70748f96a916a82f85b98cac
Martin Kirkholt Melhus <martin.melhus@gmail.com>
2016-02-02 17:44:56 +0100

    Add real world ex0

---
+++ public/ex0/realworld/rtchub.spoiler.js
@@ -0,0 +1,101 @@
+var servers = {
+    iceServers: [
+        {url:'stun:stun.l.google.com:19302'},
+        {url:'stun:stun1.l.google.com:19302'},
+        {url:'stun:stun2.l.google.com:19302'},
+        {url:'stun:stun3.l.google.com:19302'},
+        {url:'stun:stun4.l.google.com:19302'}
+    ]
+};
+
+var RTCHub = function (username, onReady, onConnectionCallback) {
+    var username = username;
+    var onReady = onReady || function () {};
+    var onConnectionCallback = onConnectionCallback || function () {};
+    var cache = {};
+
+    var websocket = io.connect("");
+    websocket.on("connect", function () {
+        onReady();
+    });
+    websocket.on("broadcast", function (dto) {
+        var message = JSON.parse(dto);
+        var source = message._source
+        // ignore messages with other destinations
+        if (message._destination !== username) return;
+        if (message.type == "offer") {
+            var offer = new RTCSessionDescription(message);
+            handleOffer(source, offer);
+        } else if (message.type == "answer") {
+            var answer = new RTCSessionDescription(message);
+            handleAnswer(source, answer);
+        }
+        else {
+            var iceCandidate = new RTCIceCandidate(message);
+            handleIceCandidate(source, iceCandidate);
+        }
+    });
+    var sendSignal = function (destination, payload) {
+        var obj = JSON.parse(JSON.stringify(payload))
+        obj["_destination"] = destination;
+        obj["_source"] = username;
+        websocket.emit("broadcast", JSON.stringify(obj));
+    }
+
+    var handleOffer = function (from, remoteDescription) {
+        var peerConnection = new RTCPeerConnection(servers);
+        peerConnection.setRemoteDescription(remoteDescription);
+        peerConnection.createAnswer(
+            function (localDescription) {
+                peerConnection.setLocalDescription(localDescription);
+                sendSignal(from, localDescription);
+
+            },
+            function (error) {
+            });
+        peerConnection.onicecandidate = function (e) {
+            if (e.candidate) {
+                sendSignal(from, e.candidate);
+            }
+        }
+        peerConnection.ondatachannel = function (e) {
+            onConnectionCallback(e.channel);
+        }
+        cache[from] = peerConnection;
+
+    }
+    var handleAnswer = function (from, remoteDescription) {
+        cache[from].setRemoteDescription(remoteDescription);
+    }
+    var handleIceCandidate = function (from, iceCandidate) {
+        cache[from].addIceCandidate(iceCandidate);
+    }
+
+    this.establishConnection = function(destination) {
+        // generate random connectionId for this connection
+        var cid = btoa((new Date().getTime() + "-" + parseInt(Math.random()*1000)));
+
+        var peerConnection = new RTCPeerConnection(servers);
+        var dataChannel = peerConnection.createDataChannel(cid, {});
+
+        peerConnection.createOffer(
+            function (offer) {
+                peerConnection.setLocalDescription(offer);
+                sendSignal(destination, offer);
+            },
+            function (error) {
+                console.error("Something went wrong", error);
+            }
+        );
+        peerConnection.onicecandidate = function (e) {
+            if (e.candidate) {
+                sendSignal(destination, e.candidate);
+            }
+        }
+        dataChannel.onopen = function (e) {
+            onConnectionCallback(dataChannel);
+        }
+        cache[destination] = peerConnection;
+    }
+};
+
